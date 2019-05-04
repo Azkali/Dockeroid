b27dc2c7a0bad4a3497a87b0c4e067be5e5f1496
Unknown <a.ffcc7@gmail.com>
2019-05-04 04:43:21 +0200

    Dirty(fix): app module quick fix

--- Back/src/app.module.ts
+++ Back/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { DockerService } from './services/docker/docker.service';
-import { AppStoreService } from './services/app-store/app-store.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [DockerService, AppStoreService],
+  providers: [DockerService],
 })
 export class AppModule {}
