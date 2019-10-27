4203e64633dc473cca44ed428231b1163093f7f8
Azkali <a.ffcc7@gmail.com>
2019-10-28 00:57:45 +0100

    fix: 🐛 Modified app-store.ts to add volumes volumes

--- Back/src/mocks/app-store.ts
+++ Back/src/mocks/app-store.ts
@@ -1,11 +1,24 @@
+import { MountConfig } from "dockerode";
+
 export interface IAppVersion<TOptions extends {}> {
 	version: string;
 	image: string;
 	options: TOptions;
+	volumes: {
+		customVolumes: boolean;
+		internalVolumes: string[];
+		userVolumes: Array<{
+			label: string;
+			desc: string;
+			target: string;
+			required: boolean;
+		}>;
+	};
 }
 
 export interface IApp<TOptions extends {}> {
 	appName: string;
+	icon: string;
 	type: string;
 	version: Array<IAppVersion<TOptions>>;
 	repository: string;
