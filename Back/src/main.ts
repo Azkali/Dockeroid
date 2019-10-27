997e6f544e03bbd72fe8d82ae93be1e0f2288e5a
Gerkin <nihilivin@gmail.com>
2019-10-27 19:19:02 +0100

    feat: 🎸 Log port on startup, default port to 4000

--- Back/src/main.ts
+++ Back/src/main.ts
@@ -6,12 +6,15 @@ config();
 
 import { NestFactory } from '@nestjs/core';
 
+import { Logger } from 'winston';
 import { AppModule } from './app.module';
 
-
 async function bootstrap() {
-  const app = await NestFactory.create( AppModule );
-  app.useLogger( app.get( 'NestWinston' ) );
-  await app.listen( 3000 );
+	const app = await NestFactory.create( AppModule );
+	app.useLogger( app.get( 'NestWinston' ) );
+
+	const port = process.env.PORT ? parseInt( process.env.PORT, 10 ) : 4000;
+	( app.get( 'NestWinston' ).logger as Logger ).info( `Starting app on port ${port}` );
+	await app.listen( port );
 }
 bootstrap();
